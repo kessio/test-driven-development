8f804de210bf4435052884d2b004384cb3b620be
unknown <sharonkessio6@gmail.com>
2022-11-24 13:27:12 +0300

    write test to raise as error incase of a negative argument

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,10 +3,17 @@ require "solver"
 describe Solver do
 
     describe ".factorial" do
+
         context "factorial of zero is one" do
           it "returns one" do
             expect(Solver.factorial(0)).to eq(1)
           end
         end
-      end
+
+        context "Only accepts zero and positive integers" do
+            it "raises an error" do
+                expect{Solver.factorial(-1)}.to raise_error(ArgumentError)
+            end
+            end
+    end
 end

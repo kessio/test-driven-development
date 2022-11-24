f6d5bcc0021faaa7db42c502bdd161bbf4ba46e4
unknown <sharonkessio6@gmail.com>
2022-11-24 13:32:59 +0300

    write code for getting factorial of a number, it passes the test

--- lib/solver.rb
+++ lib/solver.rb
@@ -2,5 +2,6 @@ class Solver
     def self.factorial(n)
         return 1 if n == 0
         raise ArgumentError, "Only accepts zero and positive integers" if n < 0
+        n * factorial(n-1)
     end
 end

9b6c5512d083aad33776b6645cfceceb044aa9f4
unknown <sharonkessio6@gmail.com>
2022-11-24 13:15:30 +0300

    fails since factorial method is not yet created

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,4 +1,14 @@
 require "solver"
 
 describe Solver do
+
+    describe ".factorial" do
+        context "factorial of zero is one" do
+          it "returns one" do
+            expect(Solver.factorial(0)).to eq(1)
+          end
+        end
+      end
+
+
 end
